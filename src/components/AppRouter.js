e4bcdf45b5f9ea4788221350b0a3a6e3dded5674
msantam2 <msantam2@gmail.com>
2017-03-07 17:34:12 -0800

    create NotFound component (404 - not found) for non-existing URLs

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Router, hashHistory, Route } from "react-router"; 
 import App from "./App"; 
 import ClientPage from './ClientPage'; 
+import NotFound from './NotFound'; 
 
 const AppRouter = () => (
   <Router history={hashHistory}>
     <Route path="/" component={App} />
     <Route path="/clients/:clientId/:clientName" component={ClientPage} />
+    <Route path="*" component={NotFound} />
   </Router>
 ); 
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../stylesheets/NotFound.css"; 
+
+const NotFound = () => (
+  <div className="not-found">
+    <p className="error-text">404 page not found</p>
+    <p className="error-text">We are sorry but the page you are looking for does not exist.</p>
+    <p className="error-text">Hint: Try the root URL (/). We still hope you have a great day!</p>
+  </div>
+); 
+
+export default NotFound; 
