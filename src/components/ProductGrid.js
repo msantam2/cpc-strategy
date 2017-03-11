33695815e94440c60db3c7110123864916b45f71
msantam2 <msantam2@gmail.com>
2017-03-11 14:29:05 -0800

    Add search bar to ProductGrid that searches for and filters products based off product name, id, or asin

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import SearchInput, { createFilter } from "react-search-input"; 
 import ProductCard from "./ProductCard"; 
 import "../stylesheets/ProductGrid.css";
+
+const KEYS_TO_FILTER = ["product_id", "product_name", "product_asin"]; 
 
 class ProductGrid extends Component {
   constructor(props) {
     super(props); 
+    this.state = { searchTerm: "" }; 
     this.generateProductCards = this.generateProductCards.bind(this); 
+    this.searchUpdated = this.searchUpdated.bind(this); 
   }
 
   generateProductCards() {
@@ -15,10 +20,28 @@ class ProductGrid extends Component {
     }); 
   }
 
+  searchUpdated(term) {
+    this.setState({ searchTerm: term });
+  }
+
   render() {
+    const { products } = this.props; 
+    const { searchTerm } = this.state; 
+    const filteredProducts = products.filter(
+      createFilter(searchTerm, KEYS_TO_FILTER)
+    ); 
+
     return (
-      <div className="product-grid">
-        {this.generateProductCards()}
+      <div>
+        <SearchInput className="search-input"
+          onChange={this.searchUpdated}
+          placeholder="Search by Product Name, ID, or ASIN"
+        />
+        <div className="product-grid">
+          {filteredProducts.map(product => {
+            return <ProductCard key={product.product_id} product={product} />;
+          })}
+        </div>
       </div>
     ); 
   }
