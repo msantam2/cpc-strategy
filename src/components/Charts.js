6dbe3e4a40d440b25e370c1fad3d1ae41ed38680
msantam2 <msantam2@gmail.com>
2017-03-12 17:09:29 -0700

    refactor ProductCard component: change #keywords to #keywordNames, eliminate instance variable avgRankPerKeyword initialized in constructor, this is called in #averagesSynopsis. Lastly, create basic scaffold of Charts component, passing down keywords as props for data manipulation to create a chart

---
+++ src/components/Charts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"; 
+
+class Charts extends Component { // this.props.keywords: [{kw1}, {kw2}, etc.]
+  render() {
+    const { keywords } = this.props; 
+    return (
+      <div>
+        whats good
+      </div>
+    ); 
+  }
+}
+
+export default Charts;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,6 +9,7 @@ import {
 } from "material-ui/Card";
 import RaisedButton from "material-ui/RaisedButton"; 
 import Dialog from "material-ui/Dialog"; 
+import Charts from "./Charts"; 
 import "../stylesheets/ProductCard.css"; 
 
 class ProductCard extends Component {
@@ -17,17 +18,15 @@ class ProductCard extends Component {
     this.state = { open: false };
     
     this.getAvgRankPerKeyword = this.getAvgRankPerKeyword.bind(this);
-    this.keywords = this.keywords.bind(this); 
     this.averagesSynopsis = this.averagesSynopsis.bind(this); 
+    this.keywordNames = this.keywordNames.bind(this); 
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
-    
-    this.avgRankPerKeyword = this.getAvgRankPerKeyword(); 
   }
 
   getAvgRankPerKeyword() {
     const { keywords } = this.props.product; // [{kw1}, {kw2}, {kw3}, etc.]
-    let averages = {}; // desired: {"foo": 54.4, "bar": 502.1, "hello": 132}
+    let averages = {}; // desired: {"foo": 54.4, "bar": 502.1, "hello": 132.0}
     for (let keyword of keywords) {
       let { ranks } = keyword; // [{rank1}, {rank2}, {rank3}, etc.]
       let avgRank = ranks.map(rank => rank.rank_position)
@@ -39,23 +38,25 @@ class ProductCard extends Component {
     return averages; 
   }
   
-  keywords() {
-    const keywordCount = Object.keys(this.avgRankPerKeyword).length;
-    return "Keywords: " + Object.keys(this.avgRankPerKeyword)
-      .join(", ")
-      .concat(` (${keywordCount})`); 
-  }
-
   averagesSynopsis() {
+    let avgRankPerKeyword = this.getAvgRankPerKeyword(); 
     let synopsis = "";
-    for (let keyword in this.avgRankPerKeyword) {
-      if (this.avgRankPerKeyword.hasOwnProperty(keyword)) {
-        let avgRank = this.avgRankPerKeyword[keyword]; 
+    for (let keyword in avgRankPerKeyword) {
+      if (avgRankPerKeyword.hasOwnProperty(keyword)) {
+        let avgRank = avgRankPerKeyword[keyword]; 
         synopsis += `${keyword}: ${avgRank.toFixed(1)} `; 
       }
     }
 
     return "Average Rankings: " + synopsis;
+  }
+
+  keywordNames() {
+    const { keywords } = this.props.product; 
+    const keywordCount = keywords.length;
+    return "Keywords: " + keywords.map(keyword => keyword.keyword_name)
+      .join(", ")
+      .concat(` (${keywordCount})`); 
   }
 
   handleOpen() {
@@ -70,13 +71,14 @@ class ProductCard extends Component {
     const { product } = this.props; 
     const productName = product.product_name; 
     const productId = product.product_id; 
-    const productASIN = product.product_asin; 
-    const keywords = this.keywords(); 
+    const productASIN = product.product_asin;
+    const keywordNames = this.keywordNames(); 
     const averagesSynopsis = this.averagesSynopsis();
     const modalAction = <RaisedButton
       label="OK"
       onTouchTap={this.handleClose}
     />; 
+    const keywords = product.keywords; 
     
     return (
       <MuiThemeProvider>
@@ -88,7 +90,7 @@ class ProductCard extends Component {
 
           <CardMedia
             overlay={<CardTitle
-              title={keywords}
+              title={keywordNames}
               subtitle={averagesSynopsis}
             />}
           >
@@ -108,7 +110,7 @@ class ProductCard extends Component {
             actions={modalAction}
             onRequestClose={this.handleClose}
           >
-            This is where the Chart component is going to go  
+            <Charts keywords={keywords} />   
           </Dialog>  
         </Card>
       </MuiThemeProvider>
